de8121190e8a22dba10cb63abb397450f57ae211
Giles Dring <giles@dringtech.com>
2023-12-07 17:47:12 +0000

    Refactor to use pathlib

--- pipelines/neet-factors/download.py
+++ pipelines/neet-factors/download.py
@@ -1,21 +1,34 @@
 import os
 import requests
+from pathlib import Path
+from urllib.parse import urlparse
 
 
-def download_url_to_file(url, local_file):
+def download_url_to_file(url, local_file=None):
+    if not local_file:
+        local_file = filename_from_url(url)
     r = requests.get(url)
     with open(local_file, 'wb') as f:
         f.write(r.content)
 
 
+def filename_from_url(url) -> str:
+    return Path(
+        urlparse(url).path
+    ).name
+
+
+def main():
+    # https://www.gov.uk/government/statistics/children-in-low-income-families-local-area-statistics-2014-to-2022
+    download_url_to_file(
+        url='https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1145380/children-in-low-income-families-local-area-statistics-2014-to-2022.ods',
+    )
+
+
 if __name__ == "__main__":
-    ROOT_DIR=os.path.realpath(os.path.join(os.path.dirname(__file__), '../../data/raw/neet-factors'))
-    os.makedirs(ROOT_DIR, exist_ok=True)
+    ROOT_DIR = Path(__file__).parent.joinpath(
+        '../../data/raw/neet-factors').resolve()
+    ROOT_DIR.mkdir(parents=True, exist_ok=True)
     os.chdir(ROOT_DIR)
 
-    # https://www.gov.uk/government/statistics/children-in-low-income-families-local-area-statistics-2014-to-2022
-    clif_url = 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1145380/children-in-low-income-families-local-area-statistics-2014-to-2022.ods'
-    download_url_to_file(
-      url=clif_url,
-      local_file=os.path.basename(clif_url)
-    )
+    main()
