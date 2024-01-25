2607d88a421f16f074c9e561efc1e361479905ec
Giles Dring <giles@dringtech.com>
2024-01-25 14:26:00 +0000

    Download and process OECD LFS by sex and age

--- pipelines/international/download.py
+++ pipelines/international/download.py
@@ -18,7 +18,13 @@ def download():
 
     # Set the current working directory
     with set_working_directory(output_dir):
-        # Download 
+        # Download LFS data
+        download_url_to_file(
+          'https://stats.oecd.org/SDMX-JSON/data/LFS_D/AUS+AUT+BEL+CAN+CHL+COL+CRI+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+OECD.MEN+WOMEN+MW.1564+1519+1524+2024+2064+2529+2534+2539+2554+2564+3034+3039+3539+3544+4044+4049+4549+4554+5054+5059+5559+5564+6064+6099+6569+6599+7074+7099+7599+7579+8099+900000.E+L+P+U.A/all?startTime=2010&endTime=2022&dimensionAtObservation=allDimensions&contentType=csv',
+          local_file='lfs_by_sex_and_age.csv'
+        )
+        
+        # Download employment rate data
         download_url_to_file(
           'https://stats.oecd.org/sdmx-json/data/DP_LIVE/.EMPAGE.../OECD?contentType=csv&detail=code&separator=comma&csv-lang=en',
           local_file='employment_rate_by_age_group.csv'
