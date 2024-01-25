b0e7527d65f4fa496b4502b5c25118fbe0b90e93
Giles Dring <giles@dringtech.com>
2024-01-25 12:35:07 +0000

    Automate downloading oecd data

---
+++ lib/downloader.py
@@ -0,0 +1,26 @@
+import requests
+from pathlib import Path
+from urllib.parse import urlparse
+
+
+def download_url_to_file(url, local_file=None):
+    '''
+    Download a url to a local file.
+    If `local_file` is not provided, attempts to work it out from the url.
+    '''
+    if not local_file:
+        local_file = filename_from_url(url)
+    # r = requests.get(url)
+    # with open(local_file, 'wb') as f:
+    #     f.write(r.content)
+        
+    with requests.get(url, stream=True) as r, open(local_file, 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=None):
+            fd.write(chunk)
+
+
+def filename_from_url(url) -> str:
+    return Path(
+        urlparse(url).path
+    ).name
+
---
+++ pipelines/international/download.py
@@ -0,0 +1,40 @@
+'''
+PYTHONPATH=. python pipelines/international/download.py
+
+Need to ensure that PYTHONPATH includes the root directory of the repo, so that it can resolve the lib
+'''
+from pathlib import Path
+from lib.downloader import download_url_to_file
+from lib.util import set_working_directory
+
+
+def download():
+    '''
+    Create working directory, then download a series of datasets into it.
+    '''
+    # Set up output directory
+    output_dir=Path(__file__).parent.joinpath('../../data/raw/oecd').resolve()
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Set the current working directory
+    with set_working_directory(output_dir):
+        # Download 
+        download_url_to_file(
+          'https://stats.oecd.org/sdmx-json/data/DP_LIVE/.EMPAGE.../OECD?contentType=csv&detail=code&separator=comma&csv-lang=en',
+          local_file='employment_by_age_group.csv'
+        )
+
+        # Download NEET
+        download_url_to_file(
+          'https://stats.oecd.org/sdmx-json/data/DP_LIVE/.NEET.../OECD?contentType=csv&detail=code&separator=comma&csv-lang=en',
+          local_file='neet.csv'
+        )
+
+        download_url_to_file(
+          'https://stats.oecd.org/sdmx-json/data/DP_LIVE/.WAGELEVEL.../OECD?contentType=csv&detail=code&separator=comma&csv-lang=en',
+          local_file='wage_levels.csv'
+        )
+
+
+if __name__ == '__main__':
+    download()
