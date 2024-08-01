4b9845876cbaaf9299a7e4f88e231bc1ba893bcb
Giles Dring <giles@dringtech.com>
2024-08-01 15:33:08 +0100

    Update most recent PCON

--- pipelines/reference/download.py
+++ pipelines/reference/download.py
@@ -22,6 +22,6 @@ with set_working_directory(ROOT / 'data/reference/'):
     # Change options, and copy the query string - alter the f=json to f=geojson
     geo="Westminster_Parliamentary_Constituencies_July_2024_Boundaries_UK_BSC"
     pcon_url = f"https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/{geo}/FeatureServer/0/query?where=1%3D1&outFields=PCON24CD,PCON24NM,PCON24NMW&outSR=4326&f=geojson"
-    pcon = load_geo_data_frame(pcon_url)
+    pcon = load_geo_data_frame(pcon_url).replace(' ', None)
     # NB Rounded to 5 decimal places
     pcon.to_file('uk-constituencies-2024.geojson', coordinate_precision=5)
